0fa41d29f8aa713cb4375ae784c58ab9e4968819
Anuj Sharma <anuj576@gmail.com>
2019-04-01 21:45:33 +1300

    More excersies solved

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,6 @@
 class Acronym
   def self.abbreviate(sentence)
-    words = sentence.split
-    acronym = words.map {|word|
-      word.split("-").map { |w| w[0].upcase  }
-      }
-    acronym.join("")
+    words = sentence.scan(/\w+/)
+    (words.map {|word| word[0].upcase}).join("")
   end
 end

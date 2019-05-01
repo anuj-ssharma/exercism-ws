e43708113e03833080a9d42dc845789207016cb6
Anuj Sharma <anuj576@gmail.com>
2019-05-01 20:43:56 +1200

    excerise updated based on feedback

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,6 +1,6 @@
 class Acronym
   def self.abbreviate(sentence)
-    words = sentence.scan(/\w+/)
-    (words.map {|word| word[0].upcase}).join("")
+    words = sentence.scan(/\b[a-zA-Z]/)
+    (words.map {|word| word[0].upcase}).join
   end
 end
